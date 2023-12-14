67936dc938c5b5251723e0c690aefb79fd8a93e4
EMPTY280_ON_DS <1123pjs@gmail.com>
2023-12-14 19:54:06 +0900

    .

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEditor;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
